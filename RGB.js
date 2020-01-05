b413a24823d4ec85e9d6e0354a551ca889a5329d
ritikasource <ritika99.work@gmail.com>
2020-01-05 23:17:51 +0530

    Changes made to js file

--- RGB.js
+++ RGB.js
@@ -1,18 +1,17 @@
 
-alert("connected");
-
 var color=[
-
 "rgb=(255, 0, 0)",
 "rgb=(0, 255, 255)",
 "rgb=(255, 0, 255)",
 "rgb=(255, 255, 0)",
 "rgb=(0, 0, 255)",
-"rgb=(0, 255, 255)",
+"rgb=(0, 255, 255)"
 
 ]
 
-var square=document.querySelector.all(".square");
+var square=document.querySelectorAll(".square");
+var pickcolor=color[2];
+var colorDisplay=document.getElementById("colorchng");
 
 for(var i=0; i<square.length; i++)
 {
