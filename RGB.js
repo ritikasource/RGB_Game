6d512eb9cb1fc775b95ac0112e4c8a05dd954f64
ritikasource <ritika99.work@gmail.com>
2020-01-06 14:59:52 +0530

    Minor cahnges done to fix the colordisplay.

--- RGB.js
+++ RGB.js
@@ -1,17 +1,19 @@
 
 var color=[
-"rgb=(255, 0, 0)",
-"rgb=(0, 255, 255)",
-"rgb=(255, 0, 255)",
-"rgb=(255, 255, 0)",
-"rgb=(0, 0, 255)",
-"rgb=(0, 255, 255)"
+
+"rgb(255, 0, 0)",
+"rgb(0, 255, 255)",
+"rgb(255, 0, 255)",
+"rgb(255, 255, 0)",
+"rgb(0, 0, 255)",
+"rgb(0, 255, 0)"
 
 ]
 
 var square=document.querySelectorAll(".square");
 var pickcolor=color[2];
-var colorDisplay=document.getElementById("colorchng");
+var colorDisplay=document.getElementById("colorDisplay");
+colorDisplay.textContent=pickcolor;
 
 for(var i=0; i<square.length; i++)
 {
@@ -20,14 +22,16 @@ for(var i=0; i<square.length; i++)
 	//event listener being added
 	square[i].addEventListener("click",function(){
     //grab the clicked color
-    var clickedColor= this.style.backgroundColor;
+    var clickedColor=this.style.backgroundColor;
     //compare it with the picked color
-    if(clickedColor===pickcolor)
-    	alert("correct!!")
+    if(clickedColor === pickcolor)
+    	{
+    		alert("correct!!");
+    	}
     else
     {
 
-    alert("wrong!!")
+    alert("wrong!!");
     }
 	});
 }
